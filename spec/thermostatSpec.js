88e2ada9e7f41cb06510b13d9432b239860c6a02
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-06-02 14:38:25 +0100

    test drive MVP

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,23 @@
+"use strict";
+
+describe("Thermostat", () => {
+  let thermostat;
+
+  beforeEach(() => {
+    thermostat = new Thermostat();
+  });
+
+  it("starts at 20 degrees", () => {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it("increases in temperature with up()", () => {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it("decreases temperature with down()", () => {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+});
